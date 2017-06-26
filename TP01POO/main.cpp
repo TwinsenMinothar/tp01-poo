b6a70535a9ba52347189048c168277dacb75e4c2
Camillo Rinaldi <camillo_rinaldi@yahoo.com.br>
2017-06-25 21:50:32 -0300

    attempt to commit

--- TP01POO/main.cpp
+++ TP01POO/main.cpp
@@ -3,12 +3,15 @@
 #include "Auxiliar.h"
 #include "Cadastro.h"
 #include <vector>
+#include <iostream>
 
 
 int main() {
 	std::vector<Aluno> alunos;
 	std::vector<Professor> professores;
 	std::vector<Auxiliar> auxiliares;
+
+	std::cout << "ive changed something bro" << std::endl;
 
 	return 0;
 }
